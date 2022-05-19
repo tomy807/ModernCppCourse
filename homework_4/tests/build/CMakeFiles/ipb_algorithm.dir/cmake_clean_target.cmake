14a6ecde4f73e8465b3e2b1de8457a53de828a66
tomy807 <tomy807@naver.com>
2022-05-19 19:41:33 +0900

    fix build and reload

--- homework_4/tests/build/CMakeFiles/ipb_algorithm.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libipb_algorithm.a"
-)
--- homework_4/tests/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tomy807/homeworks/homework_4/tests
-# Build directory: /home/tomy807/homeworks/homework_4/tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/tomy807/homeworks/homework_4/tests/build/homework_4_test_include-b12d07c.cmake")

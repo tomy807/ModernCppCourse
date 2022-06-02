4bef8de8cb3b4f98d9699aac883ea92073dacd65
leesewoong <44529918+tomy807@users.noreply.github.com>
2022-06-02 13:42:10 +0900

    complete# homework7 (#7)

---
+++ homework_7/include/homework_7.h
@@ -0,0 +1,8 @@
+#ifndef HOMEWORK_7_H_
+#define HOMEWORK_7_H_
+#include "convert_dataset.hpp"
+#include "kmeans.hpp"
+#include "serialize.hpp"
+#include "bowdictionary.hpp"
+#include <opencv2/core/mat.hpp>
+#endif
---
+++ homework_7/src/bowdictionary.cpp
@@ -0,0 +1,1 @@
+#include "../include/bowdictionary.hpp"

3c0a994c3de67f79b150161aa713479f4c8d8862
tomy807 <tomy807@naver.com>
2022-06-08 14:57:39 +0900

    Add png_strategy to read png file

--- homework_8/task_2/src/png_strategy.cpp
+++ homework_8/task_2/src/png_strategy.cpp
@@ -1,2 +1,22 @@
 #include "../include/io_strategies/png_strategy.hpp"
-igg::tmpImage igg::PngIoStrategy::ReadFromDisk(const std::string &file_name) {}
+#include <png++/png.hpp>
+igg::tmpImage igg::PngIoStrategy::ReadFromDisk(const std::string &file_name) {
+  png::image<png::rgb_pixel> img(file_name);
+  int cols = img.get_height();
+  int rows = img.get_width();
+  std::vector<tmpPixel> data;
+  int red = 0;
+  int green = 0;
+  int blue = 0;
+  for (int r = 0; r < rows; ++r) {
+    for (int c = 0; c < cols; ++c) {
+      red = img[r][c].red;
+      green = img[r][c].green;
+      blue = img[r][c].blue;
+      tmpPixel h = tmpPixel(red, green, blue);
+    data.emplace_back(h);
+    }
+  }
+  tmpImage image(rows, cols, data);
+  return image;
+}
